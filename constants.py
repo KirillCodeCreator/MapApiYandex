209c87e90e4c3729d217758d1d82d58cdbd3388a
Лоторев Кирилл <151776260+KirillCodeCreator@users.noreply.github.com>
2024-02-16 23:33:05 +0300

    Maps API Task 2 (#1)
    Co-authored-by: Kirill <okaydiscord@mail.ru>

---
+++ constants.py
@@ -0,0 +1,12 @@
+from vec import Vec
+
+# Константы для работы с API Яндекс.Карт
+MAP_API_SERVER = 'https://static-maps.yandex.ru/1.x/'
+
+MAP_TMP_FILENAME = 'map.png'
+MAP_IMG_SIZE = '600,450'
+MAP_IMG_SIZE_V = Vec(600, 450)
+
+MAP_LAYERS = ('map', 'sat', 'sat,skl')
+
+GEOCODER_APIKEY = "40d1649f-0493-4b70-98ba-98533de7710b"
