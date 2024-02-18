6ed4f2147435d8c4bddf97247e5c843c80d44f8a
Kirill <okaydiscord@mail.ru>
2024-02-18 17:11:02 +0300

    added maps Api 12

--- geocoder.py
+++ geocoder.py
@@ -1,4 +1,5 @@
 import sys
+from math import sqrt, cos, radians
 
 import requests
 
@@ -38,10 +39,12 @@ def get_toponym_spn(toponym):
     uc = [*map(float, corners["upperCorner"].split())]
     return Vec(uc[0] - lc[0], uc[1] - lc[1])
 
+
 def get_address(toponym):
     address = toponym['metaDataProperty']['GeocoderMetaData'][
         'Address']['formatted']
     return address
+
 
 def get_post_index(toponym):
     try:
@@ -50,3 +53,16 @@ def get_post_index(toponym):
     except KeyError:
         return '\nнет индекса'
     return post_index
+
+
+def lon_lat_distance(point1, point2):
+    d_to_m_factor = 111e3
+    a_lo, a_la = point1.xy
+    b_lo, b_la = point2.xy
+
+    la_lo_factor = cos(radians((a_la + b_la) / 2))
+
+    dx = abs(a_lo - b_lo) * d_to_m_factor * la_lo_factor
+    dy = abs(a_la - b_la) * d_to_m_factor
+
+    return sqrt(dx * dx + dy * dy)
--- window.py
+++ window.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import QMainWindow, QLabel, QComboBox, QLineEdit, QMessageB
 
 from constants import MAP_LAYERS, MAP_IMG_SIZE_V
 from converter import lonlat_to_xy, xy_to_lonlat, lonlat_to_spn
-from geocoder import get_toponym, get_toponym_lonlat, get_toponym_spn, get_address, get_post_index
+from geocoder import get_toponym, get_toponym_lonlat, get_toponym_spn, get_address, get_post_index, lon_lat_distance
+from search_maps import get_org_lon_lat, search_org, get_org_name, get_org_address
 from static_maps import show_map, MAP_TMP_FILENAME
 from vec import Vec
 
@@ -25,6 +26,7 @@ class Window(QMainWindow):
     post_index: QCheckBox
     dot = None
     toponym = None
+    org = None
 
     def __init__(self):
         super().__init__()
@@ -41,6 +43,7 @@ class Window(QMainWindow):
         self.address_input.textChanged[str].connect(self.address_textChanged)
         self.toponym = None
         self.dot = None
+        self.org = None
         self.zoom = 9
         self.lonlat = Vec(37.530887, 55.703118)
 
@@ -60,11 +63,18 @@ class Window(QMainWindow):
 
     def update_map(self):
         show_map(self.map_label, self.zoom, self.lonlat, self.map_type, self.dot)
+
         if self.toponym:
             address = get_address(self.toponym)
             if self.post_index.isChecked():
                 address = f'{get_post_index(self.toponym)}, {address}'
             self.full_address.setText(address)
+
+        if self.org:
+            name = get_org_name(self.org)
+            address = get_org_address(self.org)
+            # По документации API индекс в ответе отсутствует
+            self.full_address.setText(f'{name} - {address}')
 
     def closeEvent(self, event):
         os.remove(MAP_TMP_FILENAME)
@@ -129,6 +139,11 @@ class Window(QMainWindow):
                 return
             self.dot = dot
             self.update_map()
+        elif event.button() == Qt.RightButton:
+            if not self.search_org(dot, self.full_address.toPlainText()):
+                return
+            self.dot = get_org_lon_lat(self.org)
+            self.update_map()
 
     def check_borders(self):
         return not (
@@ -150,6 +165,7 @@ class Window(QMainWindow):
     def delete_search_results(self):
         self.dot = None
         self.toponym = None
+        self.org = None
         self.full_address.setText('')
         self.update_map()
 
@@ -177,3 +193,16 @@ class Window(QMainWindow):
             self.zoom -= 1
 
         self.update_map()
+
+    def search_org(self, lo_la, search_text):
+        if not search_text.strip():
+            return False
+
+        self.delete_search_results()
+        self.org = search_org(lo_la, search_text)
+
+        if self.org is None or lon_lat_distance(lo_la, get_org_lon_lat(self.org)) > 50:
+            self.org = None
+            return False
+
+        return True
